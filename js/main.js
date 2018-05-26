0889fda6f3d934b7f137a15e8045d36139d40ef6
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-26 15:40:47 +0200

    Card Slider -  Widget: Creating the JS part II - IF ELSE statement

--- js/main.js
+++ js/main.js
@@ -1,16 +1,26 @@
 $(document).ready(function() {
 
+    var itemLength = $('.cards_container .item').length;
     var current = 1;
  
     $('.cardSlider .btn_prev').on("click", function() {
-        current -= 1;
-        showSlider(current);
+        if (current > 1) {
+            current -= 1;
+            showSlider(current);
+        } else {
+            current = itemLength;
+            showSlider(current);
+        }
     })
 
     $('.cardSlider .btn_next').on("click", function() {
-
-        current += 1;
-        showSlider(current);
+        if (current !== itemLength) {
+            current += 1;
+            showSlider(current);
+        } else {
+            current = 1;
+            showSlider(current);
+        }
     })
 
     function showSlider(current) {
