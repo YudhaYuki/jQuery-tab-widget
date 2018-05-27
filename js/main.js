931d839c1c9e226a6ea36ebbc20779f1d9a3ffd5
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-27 14:13:46 +0200

    Overlay - Widget : Closing Application

--- js/main.js
+++ js/main.js
@@ -12,12 +12,22 @@ $(document).ready(function() {
 
 
     function closeOverlayAll(overlayId) {
-        //
-
-        startOverLay(overlayId)
-
-
+        $('.overlay-component').each(function() {
+            if ($(this).hasClass('active')) {
+                $('.overlay-veil').fadeOut();
+                $(this).fadeOut("300", function() {
+                    $(this).removeClass('active');
+                })
+            }
+        });
+        if (overlayId !== false) {
+            startOverLay(overlayId)
+        }
     }
+
+    $('.closeOverlay').on('click', function() {
+        closeOverlayAll(false)
+    });
 
 
     $('.overlay-item-click').on("click", function(e) {
